8ed78fbbaf7f5506fb14b9380701e9416b207c69
Ali Mersal <ali.m.mersal@gmail.com>
2026-03-27 19:49:42 +0200

    Fix blank screen on GitHub Pages and switch to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
 import BlogDetails from './pages/BlogDetails';
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router basename="/Assignment-14-Adasa/">
+    <Router>
       <div className="app-container">
         <Navbar />
         <main>
